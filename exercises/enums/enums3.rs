68fc7848a78edb689b287f005d0abf27060ad6b2
Robert Gouge <robert.c.gouge@gmail.com>
2023-01-01 19:43:45 -0600

    dual homed baby
    Signed-off-by: Robert Gouge <robert.c.gouge@gmail.com>

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,13 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8,u8,u8)),
 }
 
 struct Point {
@@ -37,8 +40,20 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::Quit => {
+                self.quit();
+            }
+            Message::Echo(_String) => {
+                self.echo(String::from("hello world"));
+            }
+            Message::Move(Point {x,y}) => {
+                self.move_position(Point{x, y});
+            }
+            Message::ChangeColor((r,b,g)) => {
+                self.change_color((r,b,g));
+            }
+        }
     }
 }
 
@@ -53,7 +68,7 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor(255, 0, 255));
+        state.process(Message::ChangeColor((255, 0, 255))); // Remember: The extra parentheses mark a tuple type.
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
